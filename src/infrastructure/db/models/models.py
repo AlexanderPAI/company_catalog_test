42e84dc8e66c2f1fa5171d05e52b0079f7937e55
AlexanderPAI <petrov@backendpages.ru>
2026-01-30 13:28:31 +0300

    Activities relationships

--- src/infrastructure/db/models/models.py
+++ src/infrastructure/db/models/models.py
@@ -1,9 +1,7 @@
-from sqlalchemy import Column, Float, String
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy import UUID, Column, Float, ForeignKey, String
+from sqlalchemy.orm import DeclarativeBase, relationship
 
 from src.infrastructure.db.models.mixins import TimestampedMixin, UUIDMixin
-
-# from sqlalchemy.orm import relationship
 
 
 class Base(DeclarativeBase):
@@ -14,6 +12,7 @@ class Building(UUIDMixin, TimestampedMixin, Base):
     """Building table"""
 
     __tablename__ = "building"
+
     address = Column(String(255), nullable=False)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
@@ -23,16 +22,22 @@ class GeneralActivity(UUIDMixin, TimestampedMixin, Base):
     """General activity table"""
 
     __tablename__ = "general_activity"
+
     title = Column(String(255), nullable=False)
-    # activity types
+
+    activity_types = relationship("ActivityType", backref="general_activity")
 
 
 class ActivityType(UUIDMixin, TimestampedMixin, Base):
     """Activity type table"""
 
     __tablename__ = "activity_type"
+
     title = Column(String(255), nullable=False)
-    # sub activities
+    sub_activities = relationship("SubActivity", backref="activity_type")
+
+    general_activity_id = Column(UUID(as_uuid=True), ForeignKey("general_activity.id"))
+    general_activity = relationship("GeneralActivity", back_populates="activity_types")
 
 
 class SubActivity(UUIDMixin, TimestampedMixin, Base):
@@ -40,6 +45,9 @@ class SubActivity(UUIDMixin, TimestampedMixin, Base):
 
     __tablename__ = "sub_activity"
     title = Column(String(255), nullable=False)
+
+    activity_type_id = Column(UUID(as_uuid=True), ForeignKey("activity_type.id"))
+    activity_type = relationship("ActivityType", backref="sub_activities")
 
 
 class Company(UUIDMixin, TimestampedMixin, Base):
