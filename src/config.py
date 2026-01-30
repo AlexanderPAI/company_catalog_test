04238d16bbdf2986a78be990b6417a2b05bb3dc2
AlexanderPAI <petrov@backendpages.ru>
2026-01-30 12:00:32 +0300

    Add postgres_url to config

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 # mypy: disable-error-code="call-arg"
+from pydantic import PostgresDsn
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
@@ -9,13 +10,24 @@ class EnvConfig(BaseSettings):
     postgres_user: str
     postgres_password: str
     postgres_host: str
-    postgres_port: str
+    postgres_port: int
     postgres_db: str
 
     model_config = SettingsConfigDict(
         env_file=".env",
         env_file_encoding="utf-8",
     )
+
+    @property
+    def postgres_url(self) -> PostgresDsn:
+        return PostgresDsn.build(
+            scheme="postgresql+asyncpg",
+            username=self.postgres_user,
+            password=self.postgres_password,
+            host=self.postgres_host,
+            port=self.postgres_port,
+            path=f"{self.postgres_db}",
+        )
 
 
 class AppConfig(BaseSettings):
