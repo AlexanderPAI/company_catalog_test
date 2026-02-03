d0042fdc27df2a8c061e45e691a7b0e344331636
AlexanderPAI <petrov@backendpages.ru>
2026-02-03 14:52:21 +0300

    Add get_company_by_building naive

--- src/interfaces/api/v1/endpoints.py
+++ src/interfaces/api/v1/endpoints.py
@@ -1,14 +1,29 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio.session import AsyncSession
 
 from src.infrastructure.db.db_connector import get_session
-from src.infrastructure.db.models.models import Company
+from src.infrastructure.db.models.models import Building, Company
 from src.infrastructure.repositories.db import DBRepository
 from src.interfaces.api.v1.schemes import CompanyScheme
 
 router = APIRouter()
+
+
+@router.get("/company/get_by_building", response_model=List[CompanyScheme])
+async def get_company_by_building(  # type: ignore
+    address: str,
+    session: AsyncSession = Depends(get_session),
+):
+    building_db_repo = DBRepository(model=Building, session=session)
+    building = await building_db_repo.get(address=address)
+    if not building:
+        raise HTTPException(status_code=404, detail="Building not found")
+
+    company_db_repo = DBRepository(model=Company, session=session)
+    companies = await company_db_repo.list(building_id=building.id)
+    return companies
 
 
 @router.get(
