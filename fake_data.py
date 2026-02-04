deef85e1a08d0f2f561476030094cab0f3762f81
AlexanderPAI <petrov@backendpages.ru>
2026-02-04 11:45:00 +0300

    Fix domain entities

---
+++ fake_data.py
@@ -0,0 +1,33 @@
+from src.domain.entities import (  # Activity,; Company,; DoubleSubActivity,; Phone,; SubActivity,
+    Building,
+)
+
+# from src.infrastructure.db.models import models
+
+building_data = [
+    Building(
+        address="Москва, ул. Братиславская, 16 к 1",
+        latitude=0.0,
+        longitude=-0.0,
+    ),
+    Building(
+        address="Москва, Кутузовский проспект, 32",
+        latitude=0.0,
+        longitude=-0.0,
+    ),
+    Building(
+        address="Москва, ул. Широкая, 24/25",
+        latitude=0.0,
+        longitude=-0.0,
+    ),
+    Building(
+        address="Москва, ул. Братиславская, 15 к 1",
+        latitude=0.0,
+        longitude=-0.0,
+    ),
+    Building(
+        address="Москва, ул. Госпитальный вал, д.3",
+        latitude=0.0,
+        longitude=-0.0,
+    ),
+]
--- src/domain/entities.py
+++ src/domain/entities.py
@@ -11,30 +11,38 @@ class Building(BaseModel):
     longitude: float
 
 
-class GeneralActivity(BaseModel):
-    """General activity entity"""
-
-    title: str
-    activity_types: List["ActivityType"]
-
-
-class ActivityType(BaseModel):
-    """Activity type entity"""
+class Activity(BaseModel):
+    """Activity entity"""
 
     title: str
     sub_activities: List["SubActivity"]
 
 
 class SubActivity(BaseModel):
+    """SubActivity entity"""
+
+    title: str
+    double_sub_activities: List["DoubleSubActivity"]
+
+
+class DoubleSubActivity(BaseModel):
     """Sub Activity entity"""
 
     title: str
+
+
+class Phone(BaseModel):
+    """Phone entity"""
+
+    phone: str
 
 
 class Company(BaseModel):
     """Company entity"""
 
     name: str
-    phone_number: List[str]
+    phones: List[Phone]
     building: Building
-    activity_type: List[ActivityType]
+    activities: List[Activity]
+    sub_activities: List[SubActivity]
+    double_sub_activities: List[DoubleSubActivity]
